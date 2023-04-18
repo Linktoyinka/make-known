246034084589938700fd2df5e0b1f8cf858f8f5a
linktoyinka <linktoyinka@gmail.com>
2023-04-18 01:37:20 +0100

    contact form validation

--- js/contact.js
+++ js/contact.js
@@ -1,11 +1,21 @@
 const submitBtn = document.getElementById("submit");
-const formText = document.querySelector(".box");
+const name = document.getElementById("name");
+const phone = document.getElementById("phone");
+const mail = document.getElementById("email");
+const msg = document.getElementById("message");
 
-formText.addEventListener("keyup", (e) => {
-    const value = e.currentTarget.value;
-    submitBtn.disabled = false;
 
-    if (value === "") {
-        submitBtn.disabled = true;
-    }
-});
+const checkBtn = () => {
+    submitBtn.disabled = !(
+        name.value && 
+        phone.value && 
+        mail.value &&
+        msg.value
+     )
+}
+
+
+name.addEventListener("keyup", checkBtn )
+phone.addEventListener("keyup", checkBtn )
+mail.addEventListener("keyup", checkBtn )
+msg.addEventListener("keyup", checkBtn )
