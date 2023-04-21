098392c55d17930ac1d04502e4781138d5fe66c9
linktoyinka <linktoyinka@gmail.com>
2023-04-21 16:47:08 +0100

    smtp update

--- js/smtp.js
+++ js/smtp.js
@@ -1,18 +1,32 @@
-Email.send({
-  SecureToken : "5b007ee8-5810-42e3-b17f-cd0945fccc02",
-  To : 'afolabi2401@gmail.com',
-  From : document.getElementById("email").value,
-  Subject : "MakeKnown New Contact Form Enquiry",
-  Body : "Name: " +
-  document.getElementById("name").value +
-  "<br> Email: " +
-  document.getElementById("email").value +
-  "<br> Phone Number: " +
-  document.getElementById("phone").value +
-  "<br> Company: " +
-  document.getElementById("company").value +
-  "<br> Message: " +
-  document.getElementById("message").value
-}).then(
-message => alert(message)
-);
+// function sendEmail() {
+
+// Email.send({
+//     SecureToken: "5b007ee8-5810-42e3-b17f-cd0945fccc02",
+//     To: "afolabi2401@gmail.com",
+//     From: document.getElementById("email").value,
+//     Subject: "MakeKnown New Contact Form Enquiry",
+//     Body:
+//       "Name: " +
+//       document.getElementById("name").value +
+//       "<br> Email: " +
+//       document.getElementById("email").value +
+//       "<br> Phone Number: " +
+//       document.getElementById("phone").value +
+//       "<br> Company: " +
+//       document.getElementById("company").value +
+//       "<br> Message: " +
+//       document.getElementById("message").value,
+//   }).then((message) => alert(message));
+// }
+
+// function sendEmail() {
+//   Email.send({
+//     Host: "smtp.elasticemail.com",
+//     Username: "username",
+//     Password: "password",
+//     To: "them@website.com",
+//     From: "you@isp.com",
+//     Subject: "This is the subject",
+//     Body: "And this is the body",
+//   }).then((message) => alert(message));
+// }
