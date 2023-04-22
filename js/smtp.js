6594e041e424787317eec5ac62c926a6eb24b54d
linktoyinka <linktoyinka@gmail.com>
2023-04-22 21:03:24 +0100

    conatct form pop-up setup and functioning

--- js/smtp.js
+++ js/smtp.js
@@ -1,4 +1,28 @@
 
+// function sendEmail() {
+//   Email.send({
+//     SecureToken: "806ed3d2-7aca-4a3e-a1b1-523a4d51e7c0",
+//     To: "afolabi2401@gmail.com",
+//     From: "linktoyinka@gmail.com",
+//     Subject: "MakeKnown New Contact Form Enquiry",
+//     Body: 
+//     "Name: " +
+//     document.getElementById("name").value +
+//     "<br> Email: " +
+//     document.getElementById("email").value +
+//     "<br> Message: " +
+//     document.getElementById("message").value + "<br> Phone: " + document.getElementById("phone").value
+//   }).then((message) => alert(message));
+// }
+
+
+
+// const form = document.getElementById('myForm');
+const modal = document.getElementById("myModal1");
+const btn = document.getElementById("modal1");
+const span = document.getElementsByClassName("close-1")[0];
+const body = document.getElementById("body");
+
 function sendEmail() {
   Email.send({
     SecureToken: "806ed3d2-7aca-4a3e-a1b1-523a4d51e7c0",
@@ -12,7 +36,27 @@ function sendEmail() {
     document.getElementById("email").value +
     "<br> Message: " +
     document.getElementById("message").value + "<br> Phone: " + document.getElementById("phone").value
-  }).then((message) => alert(message));
+  }).then((popUp));
+
+  
+
+
 }
+
+function popUp() {
+  // btn.onclick = function() {
+    modal.style.display = "block";
+    body.classList.add('popup-active')
+
+    
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+  body.classList.remove('popup-active')
+}
+
+
+
 
 
